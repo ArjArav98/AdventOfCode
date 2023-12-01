3e3448fa46155c9195cc43a8fd8aca6495e8e95f
Arjun Aravind <arjun.aravind1998@gmail.com>
2023-12-01 20:17:05 +0000

    Add 2023/ folder with content

---
+++ 2023/01A-answer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+// in a loop, we basically iterate from start and end
+// until they both meet
+
+int main() {
+    std::string line;
+
+    std::getline(std::cin, line);
+    std::cout<<line<<"+";
+
+    return 0;
+}
